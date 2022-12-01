ccad7be67ea5d89c17efedc274124c7eb18096a6
Elena <lenka.penka123412@gmail,com>
2022-12-02 02:00:05 +0300

    Метод IndexOf

---
+++ TaskMax4/Program.cs
@@ -0,0 +1,51 @@
+﻿void FiLLArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++; // index = index +1;
+    }
+}
+
+void PrintArray(int[] coll)
+
+{
+    int count = coll.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(coll[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+           position = index;
+           break;
+        }
+    index++;
+    }
+
+return position;
+}
+
+int[] array = new int[10];
+
+FiLLArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+int poss = IndexOf(array, 4);
+Console.WriteLine(poss);

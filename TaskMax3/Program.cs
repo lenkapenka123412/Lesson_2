c9b6097ea6786df16315a3acae17b246c96dfcda
Elena <lenka.penka123412@gmail,com>
2022-12-02 01:31:38 +0300

    Метод заполнения и печати массива

---
+++ TaskMax3/Program.cs
@@ -0,0 +1,25 @@
+﻿void FiLLArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++; // index = index +1;
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(coll[position]);
+        position++;
+    }
+}
+int[] array = new int[10];
+
+FiLLArray(array);
+PrintArray(array);

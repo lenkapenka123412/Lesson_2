d40ead73003bf4380820646b262102ba01f0f67c
Elena <lenka.penka123412@gmail,com>
2022-12-02 00:41:02 +0300

    Нахождение максимума с помощью аргументов

--- TaskMax1/Program.cs
+++ TaskMax1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 22;
+int b1 = 38;
+int c1 = 46;
+int a2 = 12;
+int b2 = 8556;
+int c2 = 71;
+int a3 = 14;
+int b3 = 4255;
+int c3 = 75;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);

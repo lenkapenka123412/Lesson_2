9d6d431c5651805fb002909677db604df46fcd8d
Elena <lenka.penka123412@gmail,com>
2022-12-02 01:10:49 +0300

    Начало работы с массивами

---
+++ TaskMax2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 99, 92, 33, 4, 99, 56, 47, 8, 75, 99 };
+
+int n = array.Length;
+int find = 99;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++; // index = index + 1;
+}
